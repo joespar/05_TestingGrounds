88b94a185633883399903f3fac974137aa59e65e
jvacca <jvacca@tietronix.com>
2017-08-18 16:45:41 -0500

    280, Converted the character blueprint to c++, some odd issues still need to be addressed like the weapon remaining in place after npc death.

---
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mannequin.h"
+#include "Camera/CameraComponent.h"
+#include "components/capsulecomponent.h"
+#include "components/skeletalmeshcomponent.h"
+#include "components/inputcomponent.h"
+#include "gameframework/inputsettings.h"
+#include "../Weapons/Gun.h"
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FirstPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCamera->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCamera->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
+	FirstPersonCamera->bUsePawnControlRotation = true;
+
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCamera);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	Mesh1P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+}
+
+// Called when the game starts or when spawned
+void AMannequin::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun BP missing!"))
+			return;
+	}
+	/*if (InputComponent == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InputComponent missing!"))
+			return;
+	}*/
+	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
+	//InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
+}
+
+// Called every frame
+void AMannequin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+
+void AMannequin::FireGun()
+{
+	UE_LOG(LogTemp, Warning, TEXT(" AFirstPersonCharacter::FireGun()"))
+	Gun->OnFire();
+}
